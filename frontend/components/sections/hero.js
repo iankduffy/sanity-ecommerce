3c09b8c0f626a4a70ec2d5cf1e8c0cc266b978c3
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-09-22 17:45:11 +0100

    fix image

--- frontend/components/sections/hero.js
+++ frontend/components/sections/hero.js
@@ -7,7 +7,7 @@ const HomeSplashScreen = ({heading = '', backgroundImage, tagline ='', id = ''})
 
   return(
   <section id={id} className={styles.hero} style={{backgroundImage: ``}}>
-    <ProgressImage alt={heading} previewUrl={urlFor(backgroundImage).width(500).quality(40).blur(200).auto('format').url()} imageUrl={urlFor(backgroundImage).width(1400).auto('format').url()} srcSetImages={srcSet(backgroundImage, 1800)}/>
+    <ProgressImage alt={heading} previewUrl={urlFor(backgroundImage).width(500).quality(40).blur(200).auto('format').url()} imageUrl={urlFor(backgroundImage).width(500).auto('format').url()} srcSetImages={srcSet(backgroundImage, 1800)}/>
     <div className={styles.hero__text}>
       <h1 className="">{heading}</h1>
       <p className="">{tagline}</p>
