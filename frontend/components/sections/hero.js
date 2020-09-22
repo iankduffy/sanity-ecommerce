6212cb7abb8ec4d6f6cfaa610d04e420f693cd34
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-09-22 09:52:16 +0100

    eager

--- frontend/components/sections/hero.js
+++ frontend/components/sections/hero.js
@@ -6,7 +6,7 @@ const HomeSplashScreen = ({heading = '', backgroundImage, tagline ='', id = ''})
 
   return(
   <section id={id} className={styles.hero} style={{backgroundImage: ``}}>
-    <img className="" alt={heading} srcSet={srcSet(backgroundImage, 1800)} src={urlFor(backgroundImage).width(1400).auto('format').url()}/>
+    <img className="" alt={heading} srcSet={srcSet(backgroundImage, 1800)} src={urlFor(backgroundImage).width(1400).auto('format').url()} loading="eager"/>
     <div className={styles.hero__text}>
       <h1 className="">{heading}</h1>
       <p className="">{tagline}</p>
