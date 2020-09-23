4a6b07abb3fc125888d0a049a823eb5691053a3a
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-09-23 20:54:39 +0100

    navigation

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -1,22 +1,13 @@
 import styles from '../stylesheets/components/header.module.scss'
-import Link from 'next/link';
 import NavItem from './navItem'
 
 const Nav = ({mainMenu}) => {
-  // const [mobileMenu, setMobileMenu] = useState(false)
-  console.log({mainMenu})
   return ( 
-    <div className={styles.nav}>
-      <nav className="">
-        {mainMenu?.navItems?.map((menu, key) => 
-          <NavItem key={key}  navTitle={menu?.mainpage?.title} navRoute={menu?.mainpage?.route} navDropDown={menu.dropdown} />
-          
-          // <Link key={key} href={{pathname:menu.mainpage?.route.current}}> 
-          //   <a className="o-nav--item o-header__link u-pad-v-md">{menu?.mainpage?.title}</a>
-          // </Link>
-        )}
-      </nav>
-    </div>
+    <nav className={styles.nav}>
+      {mainMenu?.navItems?.map((menu, key) => 
+        <NavItem key={key} navTitle={menu?.mainpage?.title} navRoute={menu?.mainpage?.route} navDropDown={menu?.dropdown} />
+      )}
+    </nav>
   )
 }
 
--- frontend/components/navItem.js
+++ frontend/components/navItem.js
@@ -2,24 +2,34 @@ import styles from '../stylesheets/components/header.module.scss'
 import Link from 'next/link';
 import {useState} from 'react'
 
+const DropDownItem = ({link}) => {
+  return (
+    <Link href={link?.slug?.current}>
+      <a className=''> 
+        {link.title}
+      </a>
+    </Link>
+  )
+}
 
 const DropDown = ({dropdownNav}) => { 
   return ( 
     <div className={styles.navDropDown}>
-      <div>Drop Down</div>
+      {dropdownNav.map((item, key) => <DropDownItem link={item} key={key} />)}
     </div>
   )
 }
 
 const NavItem = ({navTitle = '', navRoute = {}, navDropDown = null}) => { 
   const [isDropDownOpen, setDropDownOpen] = useState(false)
-  console.log(navDropDown)
   return ( 
     <Link href={navRoute}>
-      <a className='u-pos-relative' onMouseEnter={() => {setDropDownOpen(true)}} onMouseLeave={() => {setDropDownOpen(false)}}> 
-        {navTitle}
+      <div className='u-pos-relative' onMouseEnter={() => {setDropDownOpen(true)}} onMouseLeave={() => {setDropDownOpen(false)}}>
+        <a className="u-pad-v-md u-pad-h-md"> 
+          {navTitle}
+        </a>
         {isDropDownOpen && navDropDown && <DropDown dropdownNav={navDropDown} />}
-      </a>
+      </div>
     </Link>
   )
 }
