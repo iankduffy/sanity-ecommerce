4aec30fae62702163a465db5c533d791ab0dfdd3
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-09-28 16:04:37 +0100

    fix stupidity

--- frontend/lib/srcset.js
+++ frontend/lib/srcset.js
@@ -3,7 +3,7 @@ import urlFor from './image'
 const srcSet = (image, width) => {
   let imageSrcSet = []
 
-  for (let i = 400; i <= width; i -= 100) {
+  for (let i = 400; i <= width; i += 100) {
     let string = `${urlFor(image).width(i).quality(80).auto('format').url()} ${i}w`
     imageSrcSet.push(string)
   }
