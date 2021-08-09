c82ef030129d5ce491a55459f78a4f773e916ef0
Ian Duffy <ian.duffy@shiftcommerce.com>
2021-08-09 17:11:12 +0100

    components

--- frontend/components/sections/index.ts
+++ frontend/components/sections/index.ts
@@ -1,23 +1,5 @@
 import dynamic from 'next/dynamic'
 
-export {default as hero} from './hero'
-export {default as tabsComponents} from './tabsComponents'
-export {default as inspiration} from './inspiration'
-export {default as sixtyFortyComponent} from './SixtyFortyComponent'
-export {default as productNavigation} from './productNavigation'
-export {default as productFeatures} from './productFeatures'
-export {default as productAddToBag} from './productAddToBag'
-export {default as ctaSection} from './ctaSection'
-export {default as productTabs} from './featureTabs/productTabs'
-export {default as fullImageWithText} from './fullImageWithText'
-export {default as heroImageWithOverlay} from './heroImageWithOverlay/heroImageWithOverlay'
-export {default as sliderComp } from './sliderComp/sliderComp'
-export {default as categoriesComponent } from './categoriesComponent/categoriesComponent'
-export {default as productSlider } from './productSlider/productSlider'
-export {default as blockContentComponent } from './blockContentComp/index'
-export {default as rowImageBlockComp } from './rowImageBlockComp/index'
-
-
 export const StaticPageComponents = {
   hero: dynamic(() => import(`./hero`)), 
   tabsComponents: dynamic(() => import(`./tabsComponents`)), 
