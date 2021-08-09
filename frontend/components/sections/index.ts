d06846a0d8410b09a14de8988a497a5453d39a5d
Ian Duffy <ian.duffy@shiftcommerce.com>
2021-08-09 17:09:35 +0100

    dynamic load

--- frontend/components/sections/index.ts
+++ frontend/components/sections/index.ts
@@ -1,3 +1,5 @@
+import dynamic from 'next/dynamic'
+
 export {default as hero} from './hero'
 export {default as tabsComponents} from './tabsComponents'
 export {default as inspiration} from './inspiration'
@@ -16,4 +18,21 @@ export {default as blockContentComponent } from './blockContentComp/index'
 export {default as rowImageBlockComp } from './rowImageBlockComp/index'
 
 
-
+export const StaticPageComponents = {
+  hero: dynamic(() => import(`./hero`)), 
+  tabsComponents: dynamic(() => import(`./tabsComponents`)), 
+  inspiration: dynamic(() => import(`./inspiration`)), 
+  sixtyFortyComponent: dynamic(() => import('./SixtyFortyComponent')), 
+  productNavigation: dynamic(()=> import('./productNavigation')), 
+  productFeatures: dynamic(() => import('./productFeatures')), 
+  productAddToBag: dynamic(() => import('./productAddToBag')), 
+  ctaSection: dynamic(() => import('./ctaSection')), 
+  productTabs: dynamic(() => import('./featureTabs/productTabs')), 
+  fullImageWithText: dynamic(() => import('./fullImageWithText')), 
+  heroImageWithOverlay: dynamic(() => import('./heroImageWithOverlay/heroImageWithOverlay')), 
+  sliderComp: dynamic(() => import('./sliderComp/sliderComp')), 
+  categoriesComponent: dynamic(() => import('./categoriesComponent/categoriesComponent')), 
+  productSlider: dynamic(() => import('./productSlider/productSlider')), 
+  blockContentComponent: dynamic(() => import('./blockContentComp')),
+  rowImageBlockComp: dynamic(() => import('./rowImageBlockComp'))
+}
