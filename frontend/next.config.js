32c7666eff864858bb10701badd486d8f6c2dcfe
Ian Duffy <ian.duffy@shiftcommerce.com>
2021-08-10 16:40:39 +0100

    add bundler

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -102,3 +102,8 @@ module.exports = {
     return nextRoutes
   }
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
