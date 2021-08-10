5c142fb5bc4adb6e30eb0083b1560398853b058a
Ian Duffy <ian.duffy@shiftcommerce.com>
2021-08-10 18:35:12 +0100

    add transition to buton

--- frontend/components/sections/heroImageWithOverlay/heroImageWithOverlay.tsx
+++ frontend/components/sections/heroImageWithOverlay/heroImageWithOverlay.tsx
@@ -16,7 +16,7 @@ const heroImageWithOverlay = ({blockContent = '', image, index}: heroImageProps)
   return(
     <section className={styles.heroImageContainer}>
       <picture> 
-        <source width={getImageDimensions(image).width.toString()} height={getImageDimensions(image).height.toString()} src={urlFor(image).width(1140).height(300).fit('fill').quality(50).auto('format').url()} srcSet={srcSet(image, 1800, 700)} media="(min-width: 738px)" />
+        <source width={getImageDimensions(image).width.toString()} height={getImageDimensions(image).height.toString()} srcSet={srcSet(image, 1800, 700)} media="(min-width: 738px)" />
         <img src={urlFor(image).width(400).height(400).fit('fill').quality(50).auto('format').url()} srcSet={srcSet(image, 700)} width={getImageDimensions(image).width.toString()} height={getImageDimensions(image).height.toString()}  loading={index < 2 ? 'eager' : 'lazy'}/>
       </picture>
       <div className={styles.content}>
